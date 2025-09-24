1de23dce323fd6d76e9a94ee5242f9b5fc2f0050
Yiming <2018565194@qq.com>
2025-09-24 17:54:18 +0800

    Update AVLtree.h

--- AVLtree.h
+++ AVLtree.h
@@ -21,6 +21,35 @@ struct AVLtreeNode
 template<class K,class V>
 class AVLtree
 {
-	typedef()
-};
+	typedef AVLtreeNode<K, V> Node;
+public:
+	bool Insert(const pair<K, V>& kv)
+	{
+		if (_root == nullptr)
+		{
+			_root = new Node(key, value);
 
+			return true;
+		}
+
+		Node* parent = nullptr;
+		Node* cur = _root;
+		while (cur)
+		{
+			if (cur->_key < key)
+			{
+				parent = cur;
+				cur = cur->_right;
+			}
+			else if (cur->_key > key)
+			{
+				parent = cur;
+				cur = cur->_left;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	private:
+	};
