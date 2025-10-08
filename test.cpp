433e46540fe15db72fdc665589eba2e08c13d02a
Yiming <2018565194@qq.com>
2025-10-08 23:53:58 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -23,11 +23,30 @@ void test01()
 	//t.InOrder();
 	//cout << t._IsBalanceTree() << endl;
 }
+void test02()
+{
+	AVLtree<int, int> t;
+	int a[] = { 16, 3, 7, 11, 9, 26, 18, 14, 15 };
+	for (auto e : a)
+	{
+		if (e == 14)
+		{
+			int x = 0;
+		}
+
+		t.Insert({ e, e });
+		t.InOrder();
+		cout << "Insert:" << e << "->" << t.IsBalanceTree() << endl;
+	}
+	auto f1 = t.Find(7);
+	cout << f1->_kv.first << endl;
+}
 int main()
 {
 	//AVLtree<int, int> t;
 	//t.Insert({ 1,2 });
 	//t.Insert({ 2,3 });  
-	test01();
+	//test01();
+	test02();
 	return 0;
 }
